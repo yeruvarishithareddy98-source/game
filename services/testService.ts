cc3d9283cbfa0a3edf528f78917485a048836f8d
yeruvarishithareddy98-source <yeruvarishithareddy98@gmail.com>
2026-03-27 19:47:57 +0530

    Created testService.ts with functions to manage user progress and game attempts

---
+++ services/testService.ts
@@ -0,0 +1,21 @@
+// testService.ts
+
+export const getQuestions = async () => {
+    // Logic to get questions
+};
+
+export const saveProgress = async (userId, progress) => {
+    // Logic to save user progress
+};
+
+export const getUserProgress = async (userId) => {
+    // Logic to get user progress
+};
+
+export const getProgressBySubjectAndLevel = async (subject, level) => {
+    // Logic to get progress by subject and level
+};
+
+export const incrementAttemptsForGame = async (userId) => {
+    // Logic to increment attempts for the game
+};
